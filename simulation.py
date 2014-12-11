74b611761c551421ebdf0759da22208542f00eec
Daniel Barella <dan.barella@gmail.com>
2014-12-10 20:53:34 -0500

    Added simulation skeleton.

---
+++ simulation.py
@@ -0,0 +1,24 @@
+"""Simulation of DNA Denaturation for Statistical Mechanics Fall 2014.
+
+Runs the Metropolis algorithm and spits out... something. Not sure what yet.
+"""
+
+
+# In the order in which we sit in W114
+__author__ = [
+    'dsheets@oberlin.edu (Donal Sheets)',
+    'dbarella@oberlin.edu (Dan Barella)',
+    'simoninoc@gmail.com (Simon Lin)',
+    'dlaufer@oberlin.edu (Dan Laufer)',
+    'grichman@oberlin.edu (Gabe Richman)',
+    'mmileski@oberlin.edu (May Mileski)',
+    'cmcdonal@oberlin.edu (Coopen McDonald)',
+    ]
+
+
+def main():
+  print 'Not yet implemented.'
+
+
+if __name__ == '__main__':
+  main()
